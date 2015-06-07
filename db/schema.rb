a1cedd97c967e8c395986325b5502d4231e51628
Jon <jon@emissaryinc.com>
2015-06-06 17:48:24 -0700

    scaffolding for categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150607004601) do
+ActiveRecord::Schema.define(version: 20150607004757) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "habits", force: true do |t|
     t.integer  "category_id"
